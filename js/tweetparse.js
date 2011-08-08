8503cb10d3bdf71a666fe270d4083dc175e7d650
David Illsley <david@illsley.org>
2011-08-08 14:02:27 +0100

    Move tests out and be more careful of lone and paired *s (seen a bunch of s**t tweets get through)

--- js/tweetparse.js
+++ js/tweetparse.js
@@ -18,22 +18,6 @@ module.exports.classifyReview = function(tweet){
   return whichStar(tweet);
 }
 
-
-var tweet = "This is a five star review";
-var tweet2 = "This is a 2/5 review";
-console.log(tweet);
-var containsStar = tweet.indexOf("star") != -1  || tweet.indexOf("*") != -1;
-console.log(containsStar);
-var containsDivision = tweet2.indexOf("\/5") != -1;
-console.log(containsDivision);
-
-console.log(whichStar(tweet2));
-console.log("dottest"+"I give it 4.5 stars".match("([^\.?][1-5]|one|two|three|four|five)[ ]?(star|[\*])"));
-console.log(tweet2.match("[1-5]\/5"));
-
-var re = /((^[1-5]|[^\.?][1-5]|one|two|three|four|five)[ ]?(star|[\*])|[^\.]?[1-5]\/5)/i;
-var tweets = ["not a review", "still not a 5 anything review","pretty crappy *","slightly better **","average ***","good ****","see this *****","This is a 1/5 show","This is a 2/5 show", "this is a 3/5 show", "this is a 4/5 show", "This is a 5/5 show","I give it 1*","I give it 2*", "I give it 3*s","I give it 4*s", "I give it 5*s","I give it 1 star","I give it 2 stars","I give it 3 stars", "I give it 4 stars","I give it 5 stars","I give it one star", "I give it two stars", "I give it three stars","I give it four stars","I give it five stars","4* show"];
-
 function whichStar(tweet){
   var re = /((^[1-5]|[^\.][1-5]|one|two|three|four|five)[ ]?(star|[\*])|(^[1-5]|[^\.][1-5])\/5)/i;
   var matched = re.exec(tweet);
@@ -71,20 +55,13 @@ function whichStar(tweet){
   if(starIndex != -1){
     return 3;
   }
-  starIndex = tweet.indexOf("**");
+  starIndex = tweet.indexOf(" **");
   if(starIndex != -1){
     return 2;
   }
-  starIndex = tweet.indexOf("*");
+  starIndex = tweet.indexOf(" *");
   if(starIndex != -1){
     return 1;
   }
   return -1;
 }
-
-for(i in tweets){
-  //console.log(whichStar(tweets[i]));
-  //console.log(re.exec(tweets[i]));
-}
-
-//console.log(re.exec("I give it 4 stars"));
---
+++ js/tweetparsetest.js
@@ -0,0 +1,58 @@
+/*
+   Copyright 2011 David Illsley <david@illsley.org>
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+var tweetparse = require("./tweetparse.js");
+var tweets = {
+"not a review":-1,
+"still not a 5 anything review":-1,
+"pretty crappy *":1,
+"slightly better **":2,
+"average ***":3,
+"good ****":4,
+"see this *****":5,
+"This is a 1/5 show":1,
+"This is a 2/5 show":2,
+"this is a 3/5 show":3,
+"this is a 4/5 show":4,
+"This is a 5/5 show":5,
+"I give it 1*":1,
+"I give it 2*":2,
+"I give it 3*s":3,
+"I give it 4*s":4,
+"I give it 5*s":5,
+"I give it 1 star":1,
+"I give it 2 stars":2,
+"I give it 3 stars":3,
+"I give it 4 stars":4,
+"I give it 5 stars":5,
+"I give it one star":1,
+"I give it two stars":2,
+"I give it three stars":3,
+"I give it four stars":4,
+"I give it five stars":5,
+"4* show":4,
+"I give it 4.5 stars":-1,
+"I give it 2.5/5":-1,
+"it's a 3.5* show":-1
+};
+
+for(i in tweets)
+{
+  var result = tweetparse.classifyReview(i);
+  if(result != tweets[i]){
+  	console.log("Failure for '"+i+"' expected: "+tweets[i]+" actual: "+result);
+  }
+}
